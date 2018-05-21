4164021b2345ff20a0efc68b24b01cc9d6328fb2
9IR1AAJ1IQQZHL3\Administrator <xyj281702161@gmail.com>
2018-05-21 09:58:20 +0800

    添加项目目录

---
+++ src/test/test.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <!-- 导航栏 -->
+        <h1 v-show="true">{{ msg }}</h1>
+        <div class="navigat" v-for="(item, index) in navigat"> {{  item[index] }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+ 
+  data () {
+    return {
+      msg: 'hello world',
+      navigat: [
+          {'0': '琴'},
+          {'1': '棋'},
+          {'2': '书'},
+          {'3': '画'},
+      ],
+    }
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .container{
+        width: 90%;
+        border: 1px solid red;
+        margin: 20px auto;
+        background: url(../img/base.jpg);
+    }
+</style>
